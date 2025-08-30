957944b905793d337611af20c01b26b6c8a5bea8
chenyonghao <chenyonghao6717@yaocheng.cn>
2025-08-30 18:12:12 +0800

    write some parts of navbar and sidebar

--- src/app/dashboard-wrapper.tsx
+++ src/app/dashboard-wrapper.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import Navbar from "@/app/(components)/navbar/page";
+import Sidebar from "@/app/(components)/sidebar/page";
 
 type DashboardWrapperProps = {};
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      {/* sidebar */}
+      <Sidebar />
       <main
         className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
       >
-        {/* navbar */}
+        <Navbar />
         {children}
       </main>
     </div>
